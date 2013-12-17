e35f916aaee46448300731e49fb7fb83178d1eea
rgyang <rgyang@ctrip.com>
2013-12-17 18:03:59 +0800

    update some parameters

--- qunar/Operations/ImageThinning.cs
+++ qunar/Operations/ImageThinning.cs
@@ -10,6 +10,15 @@ namespace qunar
 {
     public class ImageThinning
     {
-
+        /// <summary>
+        /// Do image thinning and save the result in outpath.
+        /// </summary>
+        /// <param name="outpath"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static Bitmap Do_Image_Thinning(string outpath, Bitmap source)
+        {
+            return source;
+        }
     }
 }
