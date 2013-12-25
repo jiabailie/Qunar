f93abd82276b3793747b56e2e80c5a97a8484d5a
rgyang <rgyang@ctrip.com>
2013-12-25 18:19:43 +0800

    [+]update Scaling.cs

--- qunar/Operations/Scaling.cs
+++ qunar/Operations/Scaling.cs
@@ -11,12 +11,40 @@ namespace qunar
     public class Scaling
     {
         /* {0, -1, 0, -1, 5, -1, 0, -1, 0} */
-        public static void UsingSharpeningFilters(int[] kernel, Bitmap source)
+        public static void Using_Sharpening_Filters(int step, int[][] kernel, Bitmap source)
         {
+            int i = 0, j = 0;
+            int m = 0, n = 0;
+            Bitmap copy = new Bitmap(source);
 
+            try
+            {
+                for (i = 1; i < source.Width - 1; i++)
+                {
+                    for (j = 1; j < source.Height - 1; j++)
+                    {
+                        int[] rgb = new int[3] { 0, 0, 0 };
+                        for (m = -1; m <= 1; m++)
+                        {
+                            for (n = -1; n <= 1; n++)
+                            {
+                                Color point = copy.GetPixel(i + m, j + n);
+                                rgb[0] += point.R * kernel[m + 1][n + 1];
+                                rgb[1] += point.G * kernel[m + 1][n + 1];
+                                rgb[2] += point.B * kernel[m + 1][n + 1];
+                            }
+                        }
+                        source.SetPixel(i, j, Color.FromArgb(rgb[0], rgb[1], rgb[2]));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(string.Format("Scaling.Using_Sharpening_Filters:{0}", e.Message));
+            }
         }
 
-        public static Bitmap ImageZoomOut(int ratio, Bitmap source)
+        public static Bitmap Image_Zoom_Out(int ratio, Bitmap source)
         {
             if (ratio == 0) { ratio = 1; }
 
@@ -31,8 +59,6 @@ namespace qunar
 
             try
             {
-                Operations.generate_White_Edges(source);
-
                 for (i = 0; i < nWidth; i++)
                 {
                     for (j = 0; j < nHeight; j++)
@@ -59,7 +85,7 @@ namespace qunar
             }
             catch (Exception e)
             {
-                throw new Exception(string.Format("Scaling.ImageZoomOut:{0}", e.Message));
+                throw new Exception(string.Format("Scaling.Image_Zoom_Out:{0}", e.Message));
             }
             return zoomoutImage;
         }
--- qunar/Program.cs
+++ qunar/Program.cs
@@ -16,9 +16,17 @@ namespace qunar
         static void Main(string[] args)
         {
 #if DEBUG
+            int[][] template = new int[3][];
+            template[0] = new int[] { 0, -1, 0 };
+            template[1] = new int[] { -1, 5, -1 };
+            template[2] = new int[] { 0, -1, 0 };
             Bitmap source = Operations.ConvertJpg2Bmp("E:/Projects/qunar-file/input/0(64).bmp");
 
-            source = Scaling.ImageZoomOut(3, source);
+            Operations.generate_White_Edges(source);
+
+            Scaling.Using_Sharpening_Filters(3, template, source);
+
+            //source = Scaling.Image_Zoom_Out(3, source);
 
             source.Save("E:/Projects/qunar-file/test/1.bmp", ImageFormat.Bmp);
 #else
